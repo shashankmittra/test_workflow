77a89614749ce27c1dd540235b6c1f4d7e226b14
Benoit Jaunin <jaunin.b@fr.ibm.com>
2023-10-17 18:12:41 +0200

    feat: simplify reuse of sample reference scripts
    feat: simplify reuse of sample reference scripts when branch for the app source repo is not master
    https://github.ibm.com/one-pipeline/adoption-issues/issues/452

--- scripts/code_setup.sh
+++ scripts/code_setup.sh
@@ -15,7 +15,12 @@ if [[ $APP_SCM_TYPE == "gitlab" ]]; then
     "only_allow_merge_if_pipeline_succeeds": true
     }'
 else
-  curl -H "Authorization: Bearer $(cat ${APP_TOKEN_PATH})" "${APP_API_URL}/repos/${APP_REPO_OWNER}/${APP_REPO_NAME}/branches/master/protection" \
+  # If PR, then target branch of the PR is the branch to protect
+  branch=$(get_env base-branch "")
+  if [ -z "$branch" ]; then
+    branch="$(cat /config/git-branch)"
+  fi
+  curl -H "Authorization: Bearer $(cat "${APP_TOKEN_PATH}")" "${APP_API_URL}/repos/${APP_REPO_OWNER}/${APP_REPO_NAME}/branches/$branch/protection" \
     -XPUT -d '{"required_pull_request_reviews":{"dismiss_stale_reviews":true},"required_status_checks":{"strict":true,"contexts":["tekton/code-branch-protection","tekton/code-unit-tests","tekton/code-cis-check","tekton/code-vulnerability-scan","tekton/code-detect-secrets"]},"enforce_admins":null,"restrictions":null}'
 fi
 npm ci
